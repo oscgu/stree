e741f8f746833ba3081678875f9bf3fa4a4fb6d8
Oscar Guertler <oscarguertler@gmail.com>
2022-06-26 21:16:46 +0000

    align

--- config.h
+++ config.h
@@ -1,13 +1,12 @@
 /* Color theme */
 static Theme theme = {
     "\033[34;1m", /* folders */
-    "\033[90m",        /* files */
-    "\033[35m",        /* symlinks */
-    "\033[30m",        /* hardlinks */
-    "\033[36m",        /* binaries */
+    "\033[90m",   /* files */
+    "\033[35m",   /* symlinks */
+    "\033[30m",   /* hardlinks */
+    "\033[36m",   /* binaries */
 };
 
 /* Style  */
 static const char *vert = "┃";
 static const char *hori = "▁";
-
